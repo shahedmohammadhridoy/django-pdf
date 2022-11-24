6aaba866581c9d6d4204f7063cf333c7413af915
Shahed <shahed_hridoy@outlook.com>
2022-11-24 23:07:16 +0600

    first commit

---
+++ pdfapp/views.py
@@ -0,0 +1,13 @@
+from reportlab.pdfgen import canvas
+from django.http import HttpResponse
+
+
+def getpdf(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="file.pdf"'
+    p = canvas.Canvas(response)
+    p.setFont("Times-Roman", 13)
+    p.drawString(70, 780, "Hello, Shahed Mohammad Hridoy")
+    p.showPage()
+    p.save()
+    return response
